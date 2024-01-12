cbe97432d4f4eaaea8ea4bf3a111dd07a800d184
Noah Martin <noahm444@gmail.com>
2024-01-12 13:42:25 -0500

    Update checksum

--- Package.swift
+++ Package.swift
@@ -15,6 +15,6 @@ let package = Package(
         .binaryTarget(
             name: "Reaper",
             url: "https://github.com/EmergeTools/Reaper/releases/download/v1.2.0/Reaper.xcframework.zip",
-            checksum: "1467acdadd47f5b9ae4b5403f77b81412660787f6e1f08dfdf59251f6f402982"),
+            checksum: "37730b070c461e6890ebe8f9c3aeefcf51264a33aec067900c613ec99ffeb546"),
     ]
 )
